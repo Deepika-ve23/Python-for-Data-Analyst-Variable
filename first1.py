da74155698e2723a25963f12e8ab7da80b22d3df
Deepika verma <deepikavermav049@gmail.com>
2026-03-14 23:31:53 +0530

    Add files via upload

---
+++ first1.py
@@ -0,0 +1,20 @@
+print ("Hello world");
+
+# This is  a variable 
+a = 5
+print(a)
+# This is a variable
+Instructor  = 'Rishabh Mishra'
+print("python by" , Instructor, sep= '-'  )
+
+# Python as a Calculator
+print (5+3)
+print ("5 + 3")
+
+
+b = 4 
+c = 6
+print( b + c)
+print ( a+b)
+print(c / b)
+print(a * b)
